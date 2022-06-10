80f9ea8ea37b49eacc1f2c369ae317d55d23144b
Jake Lauterstein <97852239+jakelauterstein@users.noreply.github.com>
2022-06-10 15:30:01 -0500

    added password hashing bcrypt

--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+
 
 // create our User model
 class User extends Model {}
@@ -7,7 +9,6 @@ class User extends Model {}
 // define table columns and configuration
 User.init(
   {
-
  // define an id column
  id: {
     // use the special Sequelize DataTypes object provide what type of data it is
@@ -41,14 +42,25 @@ User.init(
     allowNull: false,
     validate: {
       // this means the password must be at least four characters long
-      len: [4]
+      len: [3]
     }
   }
 
   },
   {
     // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
-
+    hooks: {
+    // set up beforeCreate lifecycle "hook" functionality
+        async beforeCreate(newUserData) {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+        },
+     // set up beforeUpdate lifecycle "hook" functionality
+        async beforeUpdate(updatedUserData) {
+            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+            return updatedUserData;
+  }
+    },
     // pass in our imported sequelize connection (the direct connection to our database)
     sequelize,
     // don't automatically create createdAt/updatedAt timestamp fields
