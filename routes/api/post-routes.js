2b758e28c511371bf8a6aafd6bc1e35f6d1df57a
Jake Lauterstein <97852239+jakelauterstein@users.noreply.github.com>
2022-06-10 17:45:45 -0500

    added comment functionality

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Comment } = require('../../models');
 
 // get all posts
 router.get('/', (req, res) => {
@@ -8,6 +8,14 @@ router.get('/', (req, res) => {
         attributes: ['id', 'title', 'content', 'created_at'],
         order: [['created_at', 'DESC']], 
         include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                  }
+            },
             {
               model: User,
               attributes: ['username']
@@ -28,6 +36,14 @@ router.get('/', (req, res) => {
       },
       attributes: ['id', 'title', 'content', 'created_at'],
       include: [
+          {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+                model: User,
+                attributes: ['username']
+              }
+          },
         {
           model: User,
           attributes: ['username']
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Post, Comment } = require('../../models');
 
 // GET /api/users
 router.get('/', (req, res) => {
@@ -16,11 +16,27 @@ router.get('/', (req, res) => {
 // GET /api/users/1
 router.get('/:id', (req, res) => {
     User.findOne({
-        attributes: { exclude: ['password']},
+        attributes: { exclude: ['password'] },
         where: {
-            id: req.params.id
-        }
-    })
+          id: req.params.id
+        },
+        include: [
+          {
+            model: Post,
+            attributes: ['id', 'title', 'content', 'created_at']
+          },
+          // include the Comment model here:
+          {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'created_at'],
+            include: {
+              model: Post,
+              attributes: ['title']
+            }
+          },
+      
+        ]
+      })
         .then(dbUserData => {
             if(!dbUserData) {
                 res.status(404).json({ message: 'No user with this id'});
