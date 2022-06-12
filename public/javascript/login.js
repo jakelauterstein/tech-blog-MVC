2cbca1cd817a7e00760db8a797016f8e46092013
Jake Lauterstein <97852239+jakelauterstein@users.noreply.github.com>
2022-06-12 13:36:49 -0500

    debug signup form

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -17,6 +17,7 @@ async function signupFormHandler(event) {
       });
     // check the response status
     if (response.ok) {
+      document.location.replace('/dashboard');
         console.log('success');
       } else {
         alert(response.statusText);
